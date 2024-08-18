a20ccf9adea610cfcdc4ca844497a92758f50315
Caleb Joseph <124648231+CalebJ28@users.noreply.github.com>
2024-08-17 22:17:57 -0400

    project should be completed

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "RayTracingLearning"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "RayTracingLearning"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "RayTracingLearning"
